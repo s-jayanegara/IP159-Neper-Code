1318b5a96e6c895eb50d05ecf226efe8a2a64d39
s-jayanegara <stevenjayanegara@gmail.com>
2025-04-26 19:21:08 +0100

    Update grainproperties.py

--- grainproperties.py
+++ grainproperties.py
@@ -1,17 +1,6 @@
 grainid = open("cellid4.txt","r+")
 colour = open("graincoloursim4","w")
 
-'''
-for n in range(1,90):
-    # large grains
-    if n in (1,22,53,73,89):
-        colour.writelines(["80\n"])
-    else:
-        # small grains
-        colour.writelines(["66\n"])
-colour.close()
-'''
-
 # get second column values from grain id file
 idvalues = []
 for line in grainid:
@@ -19,9 +8,7 @@ for line in grainid:
     idvalues.append(columns[1])
 
 # assign colour to large and small grains
-# small grains = 66, large grains = 80
-# small grains = 1, large grains = 80
-# small grains = 4, large grains = 66
+# small grains = 2, large grains = 35
 for i in range(0, len(idvalues)):    
     if i == 0:
         if idvalues[i] == idvalues[i+1]:
@@ -40,5 +27,6 @@ for i in range(0, len(idvalues)):
             colour.writelines(["2\n"])
         else:
             colour.writelines(["35\n"])
+
 colour.close()
 grainid.close()
